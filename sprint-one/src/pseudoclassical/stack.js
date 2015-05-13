53decfcea688f2d0799405732274e5b6d631d4f3
jdstep <jdstep23@gmail.com>
2015-05-13 16:30:52 -0700

    returns size zero for an empty stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,11 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+
 };
 
+Stack.prototype.size = function() {
+  return 0;
+};
 
