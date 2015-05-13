16c5dac07d080e4f5b85135e7ce83cf5d663a02b
jdstep <jdstep23@gmail.com>
2015-05-13 14:44:59 -0700

    returns correct size when enqueuing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,19 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numItems = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    numItems++;
   };
 
   someInstance.dequeue = function(){
   };
 
   someInstance.size = function(){
-    return 0;
+    return numItems;
   };
 
   return someInstance;
