abb25e85960fede0b0bf4e242a426762b051f4e0
jdstep <jdstep23@gmail.com>
2015-05-13 16:15:25 -0700

    returns 0 for an empty queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,18 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+
+  storage.front = 1;
+  storage.length = 0;
+  storage.end = 0;
+
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return 0;
+};
 
