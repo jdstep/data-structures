bc229c1b021940341ea07b1146838e0edd885cb4
jdstep <jdstep23@gmail.com>
2015-05-13 14:50:44 -0700

    queue shared behavior passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,17 +4,23 @@ var Queue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
   var numItems = 0;
+  var lowestItem = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    lowestItem++;
+    storage[lowestItem] = value;
     numItems++;
   };
 
   someInstance.dequeue = function(){
-    if (numItems > 0) {
-    numItems--;
 
+    if (numItems > 0) {
+    var result = storage[lowestItem];
+    numItems--;
+    lowestItem++;
+    return result;
     }
   };
 
