ddc2556a0b5e8dda32fa87ec6c52dc1c233c733b
jdstep <jdstep23@gmail.com>
2015-05-13 15:41:20 -0700

    returns size 0

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,23 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = {};
+
+  storage.length = 0;
+  storage.front = 0;
+  storage.end = 0;
+
+  _.extend(storage, queueMethods);
+
+  return storage;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function(){
+  return 0;
+};
 
 
 
