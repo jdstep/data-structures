1318f32d3dde4230611160efa13242661f14aa5a
jdstep <jdstep23@gmail.com>
2015-05-13 16:40:03 -0700

    returns size 0 for an empty pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //
+  this.length = 0;
+  this.front = 0;
+  this.end = 0;
+};
+
+Queue.prototype.size = function (){
+  return 0;
 };
 
 
