b5ac5650e2397887bb569320e11ea5f593f01193
jdstep <jdstep23@gmail.com>
2015-05-15 19:56:53 -0700

    pseudoclassical hash table stores values

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,20 +6,56 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  this._storage.set(i, v);
+  // this._storage.set(i, v);
+
+  // retrieve k
+  //  set the result to arr
+  //  if arr is null
+  //    set arr to empty array
+  //
+  //  push v onto arr
+  //  call the helper function with arr as the new value
+
+  // var arr = this._storage.get(i);
+  //debugger;
+  //console.log(arr);
+  // if (arr === null || arr === undefined){
+  //   arr = [];
+  // }
+  // console.log(arr);
+  if (this._storage[i] === undefined) {
+    this._storage[i] = [];
+  }
+  this._storage[i].push(v);
+  //arr.push(v);
+  //this._storage.set(i, arr);
+  console.log(this._storage);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //debugger;
+  // return this._storage.get(i);
 
-  return this._storage.get(i);
+  // retrieve the value at i (an array) and store in a temporary array
+  // find in temporary array our value k
+  //var arr = this._storage.get(i);
+  // console.log(arr);
+  //var indexOfKey = _.indexOf(arr, k);
+
+  //return arr[indexOfKey];
+  // debugger;
+  var temp = this._storage[i];
+  return temp[0];
+  // return this._storage[i][0];
 
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  this._storage.set(i, null);
+  //this._storage.set(i, null);
+  delete this._storage[i];
 };
 
 
