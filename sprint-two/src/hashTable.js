390f11bc5cf940c8f681c590b1040efb960f2ffa
jdstep <jdstep23@gmail.com>
2015-05-16 11:04:43 -0700

    pseudoclassical hash tables passes all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,59 +3,46 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+// constant time with respect to number of keys
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  // this._storage.set(i, v);
+  var bucket = this._storage.get(i);
 
-  // retrieve k
-  //  set the result to arr
-  //  if arr is null
-  //    set arr to empty array
-  //
-  //  push v onto arr
-  //  call the helper function with arr as the new value
-
-  // var arr = this._storage.get(i);
-  //debugger;
-  //console.log(arr);
-  // if (arr === null || arr === undefined){
-  //   arr = [];
-  // }
-  // console.log(arr);
-  if (this._storage[i] === undefined) {
-    this._storage[i] = [];
+  if (bucket === undefined){
+    this._storage.set(i, []);
+    bucket = [];
   }
-  this._storage[i].push(v);
-  //arr.push(v);
-  //this._storage.set(i, arr);
-  console.log(this._storage);
+
+  bucket.push([k, v]);
+
+  this._storage.set(i, bucket);
 };
 
+// linear with respect to values in the bucket
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //debugger;
-  // return this._storage.get(i);
 
-  // retrieve the value at i (an array) and store in a temporary array
-  // find in temporary array our value k
-  //var arr = this._storage.get(i);
-  // console.log(arr);
-  //var indexOfKey = _.indexOf(arr, k);
-
-  //return arr[indexOfKey];
-  // debugger;
-  var temp = this._storage[i];
-  return temp[0];
-  // return this._storage[i][0];
+  var bucket = this._storage.get(i);
+  for (var x = 0; x < bucket.length; x++){
+    if (bucket[x][0] === k){
+      return bucket[x][1];
+    }
+  }
 
 };
 
+// linear with respect to values in the bucket
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  //this._storage.set(i, null);
-  delete this._storage[i];
+  var bucket = this._storage.get(i);
+
+  for (var x = 0; x < bucket.length; x++){
+    if (bucket[x][0] === k){
+      bucket[x][1] = null;
+    }
+  }
 };
 
 
